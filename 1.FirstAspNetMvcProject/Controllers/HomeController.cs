016cda58af887e9977f47a738ca8bc367112ff9d
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-15 18:12:23 +0300

    Udemy-Ilk Mvc Projem

---
+++ 1.FirstAspNetMvcProject/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1.FirstAspNetMvcProject.Controllers
+{
+    public class HomeController : Controller  //Controller classindan miras aliyor.
+    {
+        // GET: Home
+        public ActionResult Index() 
+        {
+            return View();
+        }
+
+        public ActionResult Kategoriler()
+        {
+            return View();
+        }
+    }
+}
